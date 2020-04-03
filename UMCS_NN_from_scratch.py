37b2c56ef169925eb8cc4a0ce5cba662608328ac
mattkozlowski <mateusz.a.kozlowski@gmail.com>
2020-04-03 12:51:08 +0200

    Poprawki od MK

--- UMCS_NN_from_scratch.py
+++ UMCS_NN_from_scratch.py
@@ -2,7 +2,7 @@ import numpy as np
 
 class NeuralNetwork():
     def __init__(self):
-        self.weigths = np.random.rand(6)
+        self.weigths = np.random.rand(6) #zamien na np.random.normal(size=6) -> normal oznacza ze ma byc rozklad normalny w losowaniu bo zwykly ran daje rownomierny rozklad -> kiedys wytlumacze ock
         self.bias = np.random.rand(3)
 
     def sigmoid(self, x, derivative=False):
@@ -40,42 +40,45 @@ class NeuralNetwork():
                 derivative_mse = self.mse_loss(y_value, y_predict, derivative=True)
 
                 dWeigths = np.zeros((6))
+                #Stworz sobie podobnie dBias bo to co liczysz te biasy to sa delty na biasach - wagi i biasy aktualizujemy forward w kolejnym kroku
 
                 # Deltas for weigths and biases
 
-                # MATT1: pochodne liczysz nie z wartosci wzmocnionej tylko z sumy wazonej, czyli dodaj sobie jeszcze zmienne sum wazonych ktore sobie przechowasz
+                # to nie jest self.bias[2] tylko dBias[2]                
                 self.bias[2] = self.sigmoid(O0_weighted_sum, derivative=True)
                 dWeigths[5] = H1 * self.bias[2]
                 dWeigths[4] = H0 * self.bias[2]
 
                 dH0 = dWeigths[4] * self.bias[2]
                 dH1 = dWeigths[5] * self.bias[2]
-
+                
+                # U ciebie sa inaczej wagi polaczone -> waga 0 i 2 jest polaczona do H0, a 1 i 3 do H1 stad zamienile 0,1 na 0,2 oraz 2,3 na 1,3
                 self.bias[0] = self.sigmoid(H0_weighted_sum, derivative=True)
                 dWeigths[0] = X_values[0] * self.bias[0]
-                dWeigths[1] = X_values[1] * self.bias[0]
+                dWeigths[2] = X_values[1] * self.bias[0]
 
                 self.bias[1] = self.sigmoid(H1_weighted_sum, derivative=True)
-                dWeigths[2] = X_values[0] * self.bias[1]
+                dWeigths[1] = X_values[0] * self.bias[1]
                 dWeigths[3] = X_values[1] * self.bias[1]
                 # Weigths correction
 
-                #MATT 3: wage aktualizujemy o jej rozncie
-                #w ten sposob ze self.weigths[0] -= lr * dWeigths[0] * dH0  * derivative_mse
+
+                #Zaktualizuj podobnie biasy wedlug dBias
 
                 self.weigths[0] -= lr * dWeigths[0] * dH0 * derivative_mse
                 self.weigths[1] -= lr * dWeigths[1] * dH1 * derivative_mse
                 self.weigths[2] -= lr * dWeigths[2] * dH0 * derivative_mse
                 self.weigths[3] -= lr * dWeigths[3] * dH1 * derivative_mse
+                
                 self.weigths[4] -= lr * dWeigths[4] * derivative_mse
                 self.weigths[5] -= lr * dWeigths[5] * derivative_mse
 
-                #MATT 2 to uruchom po aktualizacji wag
                 if epoch % 100 == 0:
                     result = [self.predict(x) for x in X_train]
+                    print(f'Loss {self.mse_loss(y_train, result):1.4f}\n{result}')
 
-            if epoch % 100 == 0:
-                print(f'Loss {self.mse_loss(y_train, result):1.4f}\n{result}')
+            #if epoch % 100 == 0:
+                #print(f'Loss {self.mse_loss(y_train, result):1.4f}\n{result}')
 
 
 X_train = np.array([[0, 0],
